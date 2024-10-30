e17c7eb82929063bfaea413c434ebb3c99f46a01
dineshglb <62278611+dineshglb@users.noreply.github.com>
2024-10-30 11:46:25 +0530

    Update ex1.py

--- pages/ex1.py
+++ pages/ex1.py
@@ -3,4 +3,4 @@ import pandas as pd
 import numpy as np
 
 st.header("ML")
-st.magic("Machine learning (ML) is a field of artificial intelligence where computers learn to make decisions or predictions based on data rather than being explicitly programmed with specific instructions. Essentially, it’s like training a computer to recognize patterns, adapt to new situations, and improve over time.")
+st.write("Machine learning (ML) is a field of artificial intelligence where computers learn to make decisions or predictions based on data rather than being explicitly programmed with specific instructions. Essentially, it’s like training a computer to recognize patterns, adapt to new situations, and improve over time.")
